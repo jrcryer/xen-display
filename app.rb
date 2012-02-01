984b1d0d20907986e088623e3f9ae646f615c150
James Cryer <j.r.cryer@gmail.com>
2012-02-01 00:13:40 +0000

    Adding update method

--- app.rb
+++ app.rb
@@ -4,20 +4,78 @@ module XenDisplay
 
   class Application < Sinatra::Base
 
-    get '/' do  
-      file_path = "#{settings.root}/tmp/cache"
-      @servers   = []
+    set :cache_file, File.join(settings.root, 'tmp/cache')
 
-      if File.readable?(file_path)
-        file      = File.open(file_path, "r")
-        contents  = file.read
-        @servers  = JSON.parse(contents) 
+    def cache_servers(path, content)
+      file = File.open(path, "w")
+      file.write content.to_json
+      file.close
+    end
+
+    def update_servers(servers, host, data)
+      servers[host] = []
+      guests        = data.split("\n\n\n")
+
+      guests.each do |guest|
+        next if guest.include?("Control domain")
+
+        server = get_server_detail(guest) 
+        next if server.empty?
+        
+        servers[host] << server
       end
+      servers
+    end 
+
+    def get_server_detail(guest)
+
+      server     = {}
+      attributes = guest.split("\n")
+
+      attributes.each do |attribute|
+        label, value = attribute.split(':')
+        next if label.empty? || label.include?('uuid')
+
+        label = 'name'  if label.include?('name')
+        label = 'state' if label.include?('state')
+        server[label] = value.strip
+      end
+      server
+    end
+
+    before do
+      unless File.readable?(settings.cache_file) and File.writable?(settings.cache_file)
+        File.open(settings.cache_file, 'w')
+      end
+    end
+
+    get '/' do  
+      file      = File.open(settings.cache_file, "r")
+      content   = file.read
+      @servers  = content.empty? ? {} : JSON.parse(content) 
+      file.close
+
       erb :index
     end
 
-    post '/update' do
-    	
+    post '/update/:host' do
+      
+      host = params[:host]
+      data = params[:data]
+
+      if data.empty?
+          raise "Invalid parameters"
+      end
+      file    = File.open(settings.cache_file, "r")
+      content = file.read
+      servers = content.empty? ? {} : JSON.parse(content)
+      file.close
+
+      servers = update_servers(servers, host, data)
+      cache_servers(settings.cache_file, servers)
+
+      content_type :text
+      puts 'complete'
     end
 
   end
