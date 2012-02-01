fbe09b36fc532ec8e00b1ad4ff6eec7b7b784cc6
James Cryer <j.r.cryer@gmail.com>
2012-02-01 00:45:24 +0000

    Cleaning up app

--- app.rb
+++ app.rb
@@ -1,53 +1,10 @@
 require 'json'
+require_relative 'lib/xenfilestore'
 
 module XenDisplay
 
-  class Application < Sinatra::Base
-
-    set :cache_file, File.join(settings.root, 'tmp/cache')
-
-    def cache_servers(path, content)
-      file = File.open(path, "w")
-      file.write content.to_json
-      file.close
-    end
-
-    def update_servers(servers, host, data)
-      servers[host] = []
-      guests        = data.split("\n\n\n")
-
-      guests.each do |guest|
-        next if guest.include?("Control domain")
-
-        server = get_server_detail(guest) 
-        next if server.empty?
-        
-        servers[host] << server
-      end
-      servers
-    end 
-
-    def get_server_detail(guest)
-
-      server     = {}
-      attributes = guest.split("\n")
-
-      attributes.each do |attribute|
-        label, value = attribute.split(':')
-        next if label.empty? || label.include?('uuid')
-
-        label = 'name'  if label.include?('name')
-        label = 'state' if label.include?('state')
-        server[label] = value.strip
-      end
-      server
-    end
-
-    before do
-      unless File.readable?(settings.cache_file) and File.writable?(settings.cache_file)
-        File.open(settings.cache_file, 'w')
-      end
-    end
+  class Application < Sinatra::Base    
+    register Sinatra::XenFileStore
 
     get '/' do  
       file      = File.open(settings.cache_file, "r")
@@ -77,7 +34,7 @@ module XenDisplay
       content_type :text
       puts 'complete'
     end
-
+    
   end
 
 end
---
+++ lib/xenfilestore.rb
@@ -0,0 +1,60 @@
+require 'sinatra/base'
+
+module Sinatra
+	module XenFileStore
+
+		module Helpers
+			def cache_servers(path, content)
+      	file = File.open(path, "w")
+      	file.write content.to_json
+      	file.close
+    	end
+
+    	def update_servers(servers, host, data)
+      	servers[host] = []
+      	guests        = data.split("\n\n\n")
+
+      	guests.each do |guest|
+        	next if guest.include?("Control domain")
+
+        	server = get_server_detail(guest) 
+        	next if server.empty?
+        
+        	servers[host] << server
+      	end
+      	servers
+    	end 
+
+    	protected 
+    	
+    	def get_server_detail(guest)
+
+      	server     = {}
+      	attributes = guest.split("\n")
+
+      	attributes.each do |attribute|
+        	label, value = attribute.split(':')
+        	next if label.empty? || label.include?('uuid')
+
+        	label = 'name'  if label.include?('name')
+        	label = 'state' if label.include?('state')
+        	server[label] = value.strip
+      	end
+      	server
+    	end
+    end
+
+  	def self.registered(app)
+  		app.helpers XenFileStore::Helpers
+  		app.set :cache_file, File.join(settings.root, 'tmp/cache')
+
+      app.before {
+        unless File.readable?(settings.cache_file) and File.writable?(settings.cache_file)
+          File.open(settings.cache_file, 'w')
+        end
+      }
+		end
+	end
+
+	register XenFileStore
+end
